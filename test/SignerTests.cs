357558ce44a64f49e9ab049cd44a46b41b75c281
stejin <steffen@stejin.org>
2018-10-31 17:51:58 -0700

    Fixed tests

--- test/SignerTests.cs
+++ test/SignerTests.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ProD.Integration.Blockchain.Ethereum;
 
 namespace ProDerivatives.Ethereum.Test
 {
