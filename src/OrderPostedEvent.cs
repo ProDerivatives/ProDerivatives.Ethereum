5b540a02590cea58b1347ae51cc67058097d3d60
Steffen <steffen@stejin.org>
2019-02-28 18:02:51 -0800

    Added Order and Trade Events

---
+++ src/OrderPostedEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Nethereum.ABI.FunctionEncoding.Attributes;
+
+namespace ProDerivatives.Ethereum
+{
+    public class OrderPostedEvent
+    {
+        [Parameter("address", "account", 1, false)]
+        public string Account { get; set; }
+
+        [Parameter("int", "notional", 2, false)]
+        public int Notional { get; set; }
+
+        [Parameter("int", "xp", 3, false)]
+        public int Price { get; set; }
+    }
+}
---
+++ src/TradeClearedEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Nethereum.ABI.FunctionEncoding.Attributes;
+
+namespace ProDerivatives.Ethereum
+{
+    public class TradeClearedEvent
+    {
+        [Parameter("address", "longAccount", 1, false)]
+        public string LongAccount { get; set; }
+
+        [Parameter("address", "shortAccount", 2, false)]
+        public string ShortAccount { get; set; }
+
+        [Parameter("int", "notional", 3, false)]
+        public int Notional { get; set; }
+
+        [Parameter("int", "xp", 4, false)]
+        public int Price { get; set; }
+    }
+}
